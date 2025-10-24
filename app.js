d7f0f4eeceab95a68d8797ca98f3180a5c433168
Threshik <threshikavijay@gmail.com>
2025-10-24 11:08:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const res = require("./data") 
+console.log(res)
+
+function sayHello(name){
+    console.log("Hello "+name);
+}
+sayHello("Threshi")
---
+++ data.js
@@ -0,0 +1,16 @@
+names=["apple","mango","kiwi"]
+age=[23,56,78,90]
+console.log(names)
+
+
+module.exports.list = {
+    names: names,
+    age: age
+}
+// module.exports = {
+//     names: names,
+//     age: age
+// }
+//module.exports = names; 
+//module.exports = "threshi"
+//module.exports.names = names -> returns as a object
