8d62883eb1c3d08058225061271f4b19f4104f40
Threshik <threshikavijay@gmail.com>
2025-10-24 11:11:26 +0530

    exporting multiple values without field names

--- data.js
+++ data.js
@@ -2,11 +2,13 @@ names=["apple","mango","kiwi"]
 age=[23,56,78,90]
 console.log(names)
 
-
-module.exports.list = {
-    names: names,
-    age: age
-}
+module.exports={
+    names,age
+};
+// module.exports.list = {
+//     names: names,
+//     age: age
+// }
 // module.exports = {
 //     names: names,
 //     age: age
